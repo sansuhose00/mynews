76782eb92b59750fd5f61ee7cd4d5eae6af3071f
octopus <torres@DESKTOP-QML9V1U.localdomain>
2021-10-06 13:22:37 +0900

    laraver12

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.profile')
+
+@section('title','プロフィール')
+
+@section('content')
+     <div class="container">
+       <div class="row">
+         <div class="col-md-8 mx-auto">
+           <h2>プロフィール</h2>
+         </div>
+       </div>
+     </div>
+@endsection
